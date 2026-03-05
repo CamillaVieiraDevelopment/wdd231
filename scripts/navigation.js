d4f46d9fff0dd6188fe80a83e260c5dddbe00994
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-03-05 16:56:32 -0300

    initial commit

---
+++ scripts/navigation.js
@@ -0,0 +1,9 @@
+const navButton = document.querySelector("#nav-button");
+const navMenu = document.querySelector(".navigation");
+
+navButton.addEventListener("click", () => {
+
+    navButton.classList.toggle("show");
+    navMenu.classList.toggle("show");
+
+});
