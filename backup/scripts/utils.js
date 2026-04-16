b7fd4afaac66090d32f48acf924b1482bd6db2aa
CamillaVieiraDevelopment <camillafranciannesrv@gmail.com>
2026-04-16 11:08:21 -0300

    initial commit

--- backup/scripts/utils.js
+++
@@ -1,9 +0,0 @@
-/* scripts/utils.js - Shared Utility Functions */
-
-export function getFormattedDate() {
-    return new Date().getFullYear();
-}
-
-export function updateLastModified() {
-    return `Last Modification: ${document.lastModified}`;
-}
